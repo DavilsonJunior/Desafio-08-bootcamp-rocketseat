048141de92bb45fe0c1de7236f0e1cdb703ee39f
DavilsonJunior <davilsonjr_jobs@yahoo.com>
2022-05-13 01:00:56 -0300

    add increment and decrement items on the card

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -53,13 +53,33 @@ const CartProvider: React.FC = ({ children }) => {
     [products],
   );
 
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+  const increment = useCallback(
+    async id => {
+      setProducts(
+        products.map(p =>
+          p.id === id ? { ...p, quantity: p.quantity + 1 } : p,
+        ),
+      );
+    },
+    [products],
+  );
 
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+  const decrement = useCallback(
+    async id => {
+      const newProducts = products.find(p => p.id === id);
+
+      if (newProducts?.quantity === 1) {
+        setProducts(products.filter(p => p.id !== id));
+      } else {
+        setProducts(
+          products.map(p =>
+            p.id === id ? { ...p, quantity: p.quantity - 1 } : p,
+          ),
+        );
+      }
+    },
+    [products],
+  );
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
